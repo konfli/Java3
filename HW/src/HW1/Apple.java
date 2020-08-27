a4c85e78934da272368760936aef03762f9500d9
Black_cat <ichernok@gmail.com>
2020-08-27 10:41:12 +0300

    HW1

---
+++ HW/src/HW1/Apple.java
@@ -0,0 +1,15 @@
+package HW1;
+
+public class Apple extends Fruit {
+
+
+    @Override
+    String getName() {
+        return "Apple";
+    }
+
+    @Override
+    double getWeight() {
+        return 1.0;
+    }
+}
---
+++ HW/src/HW1/Orange.java
@@ -0,0 +1,15 @@
+package HW1;
+
+public class Orange extends Fruit {
+
+
+    @Override
+    String getName() {
+        return "Orange";
+    }
+
+    @Override
+    double getWeight() {
+        return 1.5;
+    }
+}
